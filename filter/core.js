280bbd208c155a4826f6f10b72d72a4d450a2f9d
Ji Kim <jkim55@gmail.com>
2016-09-06 23:11:30 -0600

    completed filter

--- filter/core.js
+++ filter/core.js
@@ -1,16 +1,31 @@
-function onlyEven (array) {
-  // your code here
+function onlyEven (arr) {
+  let new_arr = arr.filter((element)=>{
+    return (element % 2 === 0)
+  })
+  return new_arr
 };
 
-function onlyOneWord (array) {
-  // your code here
+function onlyOneWord (arr) {
+  let new_arr = arr.filter((words)=>{
+    return words.split(" ").length === 1
+  })
+  return new_arr
 };
 
-function positiveRowsOnly (array) {
-  // your code here
-};
+function positiveRowsOnly (nestedArr) {
+  let new_nestedArr = nestedArr.filter((arr)=>{
+    var returnVal = true
+    for(var i=0; i<arr.length; i++){
+      if (arr[i] < 0){
+        returnVal = false
+      }
+    }
+    return returnVal
+  })
+  return new_nestedArr
+}
 
-function allSameVowels (array) {
+function allSameVowels (arr) {
   // your code here
 };
 
