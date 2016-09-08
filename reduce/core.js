169f56b1e6bcb95d27e5bcbe97dd3dcc57b969cc
Ji Kim <jkim55@gmail.com>
2016-09-07 21:59:41 -0600

    completed filter

--- reduce/core.js
+++ reduce/core.js
@@ -1,18 +1,46 @@
-function sum (array) {
-  // your code here
+function sum (arr) {
+  return arr.reduce((pVal, cVal)=>{
+    return pVal + cVal
+  })
 };
 
 function productAll (array) {
-  // your code here
+  return array.reduce(function (product, row) {
+    product *= row.reduce(function (prev, curr) {
+      return prev * curr;
+    });
+
+    return product;
+  }, 1);
+}
+
+function productAll (parentArr) {
+  return parentArr.reduce((total, element)=>{
+    total *= element.reduce((pVal, cVal)=>{
+      return pVal * cVal
+    })
+    return total
+  }, 1)
 };
 
-function objectify (array) {
-  // your code here
+function objectify (parentArr) {
+  return parentArr.reduce(function (childArr, element) {
+    childArr[element[0]] = element[1];
+    return childArr;
+  }, {});
+}
+
+function luckyNumbers (arr) {
+  return arr.reduce((string, num, idx, stringArr)=>{
+    if (idx !== stringArr.length -1){
+      return string += (num + ', ')
+    } else {
+      console.log(stringArr);
+      return string += ('and ' + num)
+    }
+  }, 'Your lucky numbers are: ')
 };
 
-function luckyNumbers (array) {
-  // your code here
-};
 
 module.exports = {
   sum: sum,
